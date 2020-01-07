f00ffc4624f959706226d0622a1a2a50fd6e6991
trevor-ofarrell <744@holbertonschool.com>
2020-01-06 21:16:44 -0800

    working with postman

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import cors from 'cors';
 import express from 'express';
+const csvjson = require('csvjson');
+const readFile = require('fs').readFile;
+const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+  console.log('server runnning on port 3000!'),
 );
 app.get('/', (req, res) => {
   return res.send('Received a GET HTTP method');
@@ -16,4 +20,19 @@ app.put('/', (req, res) => {
 });
 app.delete('/', (req, res) => {
   return res.send('Received a DELETE HTTP method');
+});
+app.post("/convert", (req, res, next) => {
+    readFile('./test-data.csv', 'utf-8', (err, fileContent) => {
+	if(err) {
+            console.log(err); // Do something to handle the error or just throw it
+            throw new Error(err);
+	}
+	req.body.content = csvjson.toObject(fileContent);
+	console.log(req.body.content);
+        if(req.body.content == null) {
+            res.json(["error", "No data found damnit"]);
+	} else {
+	    res.json(["json", req.body.content]);
+	}
+    });
 });
