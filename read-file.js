9d56ca5dfd6ecb482976ab2175c396a4e33744ab
trevor-ofarrell <744@holbertonschool.com>
2020-01-07 02:28:30 -0800

    containerizing API

--- read-file.js
+++
@@ -1,9 +0,0 @@
-const createReadStream = require('fs').createReadStream;
-const createWriteStream = require('fs').createWriteStream;
-const csvjson = require('csvjson');
-const toObject = csvjson.stream.toObject();
-const stringify = csvjson.stream.stringify();
-createReadStream('./test-data.csv', 'utf-8')
-    .pipe(toObject)
-    .pipe(stringify)
-    .pipe(createWriteStream('./test-data-output-stream.json'));
