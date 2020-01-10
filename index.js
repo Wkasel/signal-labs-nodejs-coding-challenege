dd5cb72aac7dc6d8e0810976576affb401e9fd06
trevor-ofarrell <744@holbertonschool.com>
2020-01-10 13:15:00 -0800

    linting

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import cors from 'cors';
 
 import express from 'express';
 
-import { createReadStream } from 'fs';
-
 const csvjson = require('csvjson');
 const { readFile } = require('fs');
 const bodyParser = require('body-parser');
@@ -31,23 +29,23 @@ app.delete('/', (req, res) => res.send('Received a DELETE HTTP method'));
 
 // HTTP post method for converting file to JSON, populated by html form
 app.post('/convert', (req, res) => {
-    // read from CSV test file in project dir
-    readFile(String(req.body.file), 'utf-8', (err, fileContent) => {
-	// If error occors during reading of file, display error msg
-	if (err) {
-	    console.log(err);
-	    throw new Error(err);
-	}
-	// convert file contents to json and set as request body contents
-	req.body.content = csvjson.toObject(fileContent);
-	// if filepath is null
-	if (req.body.content == null) {
-	    res.json(['error', 'No data found']);
-	}	
-	// return json
-	else {
-	    res.json(['json', req.body.content]);
-	    console.log(req.body.content);
-	}
-    });
+  // read from CSV test file in project dir
+  readFile(String(req.body.file), 'utf-8', (err, fileContent) => {
+    // If error occors during reading of file, display error msg
+    if (err) {
+      console.log(err);
+      throw new Error(err);
+    }
+    // convert file contents to json and set as request body contents
+    req.body.content = csvjson.toObject(fileContent);
+    // if filepath is null
+    if (req.body.content == null) {
+      res.json(['error', 'No data found']);
+    }
+    // return json
+    else {
+      res.json(['json', req.body.content]);
+      console.log(req.body.content);
+    }
+  });
 });
