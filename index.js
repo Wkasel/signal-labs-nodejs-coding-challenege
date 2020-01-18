d75e6ce33edb8d46ef7db31f6a4131c4771c2a29
trevor-ofarrell <744@holbertonschool.com>
2020-01-17 22:58:34 -0800

    linting

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded());
 app.use(express.static('static'));
 
 // set port number to listen with
-app.listen(3000, () => console.log('server runnning on port 3000!'));
+app.listen(3000);
 
 // Allowed HTTP methods
 app.get('/', (req, res) => res.send('Received a GET HTTP method'));
@@ -40,15 +40,17 @@ app.delete('/', (req, res) => res.send('Received a DELETE HTTP method'));
 // HTTP post method for converting file to JSON, populated by html form
 app.post('/convert', upload.single('file'), (req, res) => {
   // if file exists convert to JSON and return, if conversion fails send 400 error code
+  let ret = null;
   if (req.file) {
     readFile(String(req.file.path), 'utf-8', (err, fileContent) => {
       const jsonobj = csvjson.toObject(fileContent);
       if (jsonobj) {
-        return res.json([jsonobj]);
+        ret = res.json([jsonobj]);
       }
       if (err) {
-        return res.send(400);
+        ret = res.send(400);
       }
     });
   }
+  return ret;
 });
